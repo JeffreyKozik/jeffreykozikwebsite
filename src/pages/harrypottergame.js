a6b98bd12d8ad37ac8ac1298eb5bb97c5c622560
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-03 18:36:56 -0500

    harry potter game

--- src/pages/harrypottergame.js
+++ src/pages/harrypottergame.js
@@ -1,10 +1,4 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import HomePage from "../components/homepage"
 
 // const IndexPage = () => (
 //   <Layout>
@@ -30,8 +24,8 @@ import HomePage from "../components/homepage"
 //   </Layout>
 // )
 
-const IndexPage = () => (
-    <HomePage/>
+const HarryPotterGame = () => (
+    <div> Harry Potter Game </div>
 )
 
-export default IndexPage
+export default HarryPotterGame
