ed908c5c9186387dba6a0d57da93d6c313d3fea1
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-03 18:32:57 -0500

    let's try react-select again

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `react-select`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
