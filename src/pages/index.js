556be1cc857b0ddc73b01bb6053eca477c87771e
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-03 17:42:35 -0500

    changed index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,32 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import HomePage from "../components/homepage"
 
+// const IndexPage = () => (
+//   <Layout>
+//     <Seo title="Home" />
+//     <Homepage/>
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <StaticImage
+//       src="../images/gatsby-astronaut.png"
+//       width={300}
+//       quality={95}
+//       formats={["auto", "webp", "avif"]}
+//       alt="A Gatsby astronaut"
+//       style={{ marginBottom: `1.45rem` }}
+//     />
+//     <p>
+//       <Link to="/page-2/">Go to page 2</Link> <br />
+//       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+//       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+//       <Link to="/using-dsg">Go to "Using DSG"</Link>
+//     </p>
+//   </Layout>
+// )
+
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <Homepage/>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
+    <div> Hi </div>
 )
 
 export default IndexPage
