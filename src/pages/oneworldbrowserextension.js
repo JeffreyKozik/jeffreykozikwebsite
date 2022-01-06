325ebd760713bdb888ca99e493bc7bc834e85a7a
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-06 18:29:22 -0500

    trying to get video to display

--- src/pages/oneworldbrowserextension.js
+++ src/pages/oneworldbrowserextension.js
@@ -2,15 +2,25 @@ import * as React from "react"
 import "../../node_modules/video-react/dist/video-react.css"
 import { Player } from 'video-react';
 
-const OneWorldBrowserExtension = () => (
-    <>
-        <div> One World Browser Extension </div>
+// const OneWorldBrowserExtension = () => (
+//     <>
+//         <div> One World Browser Extension </div>
+//         <Player
+//             playsInline
+//             poster="../images/gatsby-icon.png"
+//             src="../images/1world.mp4"
+//         />
+//     </>
+// )
+//
+// export default OneWorldBrowserExtension
+
+export default props => {
+    return (
         <Player
             playsInline
             poster="../images/gatsby-icon.png"
             src="../images/1world.mp4"
         />
-    </>
-)
-
-export default OneWorldBrowserExtension
+    );
+};
