6442c1ae31e9db9e24b4dcd8bb63f7d85a9779ee
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-06 18:50:24 -0500

    center

--- src/pages/oneworldbrowserextension.js
+++ src/pages/oneworldbrowserextension.js
@@ -1,34 +1,18 @@
 import * as React from "react"
-import "../../node_modules/video-react/dist/video-react.css"
-import { Player } from 'video-react';
 import demoVideo from "../images/1world.mp4";
+import "../page_styling/oneworldbrowserextension.css"
+import Seo from "../components/seo"
 
 const OneWorldBrowserExtension = () => (
     <>
-        <div> One World Browser Extension </div>
-        <video controls>
-            <source src={demoVideo} type="video/mp4"></source>
-        </video>
+        <Seo title="One World Browser Extension" />
+        <div id="one_world_browser_extension_title"> One World Browser Extension </div>
+        <div class="container">
+            <video controls>
+                <source src={demoVideo} type="video/mp4"></source>
+            </video>
+        </div>
     </>
 )
 
-// const OneWorldBrowserExtension = () => (
-//     <>
-//         <div> One World Browser Extension </div>
-//         <video controls>
-//             <source src="../images/1world.mp4" type="video/mp4">
-//         </video>
-//     </>
-// )
-
 export default OneWorldBrowserExtension
-
-// export default props => {
-//     return (
-//         <Player
-//             playsInline
-//             poster="../images/gatsby-icon.png"
-//             src="../images/1world.mp4"
-//         />
-//     );
-// };
