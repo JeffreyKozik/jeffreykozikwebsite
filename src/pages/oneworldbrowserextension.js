1b08bdafc03036f5d5bf28522d3079808701ee15
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-06 18:40:40 -0500

    trying something new

--- src/pages/oneworldbrowserextension.js
+++ src/pages/oneworldbrowserextension.js
@@ -1,15 +1,25 @@
 import * as React from "react"
 import "../../node_modules/video-react/dist/video-react.css"
 import { Player } from 'video-react';
+import demoVideo from "../images/1world.mp4";
 
 const OneWorldBrowserExtension = () => (
     <>
         <div> One World Browser Extension </div>
         <video controls>
-            <source src="gatsby-starter-default/src/images/1world.mp4" type="video/mp4">
+            <source src={demoVideo} type="video/mp4"></source>
         </video>
     </>
 )
+
+// const OneWorldBrowserExtension = () => (
+//     <>
+//         <div> One World Browser Extension </div>
+//         <video controls>
+//             <source src="../images/1world.mp4" type="video/mp4">
+//         </video>
+//     </>
+// )
 
 export default OneWorldBrowserExtension
 
