0abd1a35f6cd6e105034b12fc4fcce7c32dd86fb
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-06 18:32:51 -0500

    just using video tag

--- src/pages/oneworldbrowserextension.js
+++ src/pages/oneworldbrowserextension.js
@@ -2,25 +2,23 @@ import * as React from "react"
 import "../../node_modules/video-react/dist/video-react.css"
 import { Player } from 'video-react';
 
-// const OneWorldBrowserExtension = () => (
-//     <>
-//         <div> One World Browser Extension </div>
+const OneWorldBrowserExtension = () => (
+    <>
+        <div> One World Browser Extension </div>
+        <video controls>
+            <source src="../images/1world.mp4">
+        </video>
+    </>
+)
+
+export default OneWorldBrowserExtension
+
+// export default props => {
+//     return (
 //         <Player
 //             playsInline
 //             poster="../images/gatsby-icon.png"
 //             src="../images/1world.mp4"
 //         />
-//     </>
-// )
-//
-// export default OneWorldBrowserExtension
-
-export default props => {
-    return (
-        <Player
-            playsInline
-            poster="../images/gatsby-icon.png"
-            src="../images/1world.mp4"
-        />
-    );
-};
+//     );
+// };
