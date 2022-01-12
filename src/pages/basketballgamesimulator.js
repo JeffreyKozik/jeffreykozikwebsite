5a1e9804bd42cb27ed6132a0a9dff90d589e92ca
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-12 12:55:21 -0500

    khan academy embed edits

--- src/pages/basketballgamesimulator.js
+++ src/pages/basketballgamesimulator.js
@@ -15,7 +15,14 @@ const BasketballGameSimulator = () => (
                     Khan Academy JavaScript project I created in 2016 to simulate basktball games
                     using data from <a href="https://www.basketball-reference.com/"> basketball-reference.com </a>.
                 </h3>
-                <h2 class="page_video"><a href="https://www.khanacademy.org/computer-programming/basketball-game/6687774851858432">Basketball Game</a></h2><script src="https://www.khanacademy.org/computer-programming/basketball-game/6687774851858432/embed.js?editor=yes&buttons=yes&author=yes&embed=yes"></script><p>Made using: <a href="http://www.khanacademy.org/computer-programming">Khan Academy Computer Science</a></p>
+                <script src="https://www.khanacademy.org/computer-programming/basketball-game/6687774851858432/embed.js?editor=yes&buttons=yes&author=yes&embed=yes"></script><p>Made using: <a href="http://www.khanacademy.org/computer-programming">Khan Academy Computer Science</a></p>
+                <p class="page_description">
+                    To run a simluation click on the right hand side of the embed which shows a mock
+                    basketball court and some circles representing players. Then press "a" on your keyboard to see the action unfold.
+                    If you want to run the simulation quickly simply hold down "a" on your keyboard. If you want to
+                    change the teams playing, simply edit the name shown on line 167 and 168 of the code to one of the other
+                    team names from the previous lines.
+                </p>
                 <p class="page_description">
                     This was one of the first projects I made when I learned how to code. It's written
                     entirely in JavaScript. It also includes the NBA team with the longest streak without losing
