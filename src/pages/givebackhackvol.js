4a4f79ebbdb39d0a4f9e783dc82bf798e6b8d2e2
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-12 22:59:15 -0500

    givebackhackvol

--- src/pages/givebackhackvol.js
+++ src/pages/givebackhackvol.js
@@ -2,9 +2,6 @@ import * as React from "react"
 import "../page_styling/pages.css"
 import Seo from "../components/seo"
 
-// <iframe src="https://codesandbox.io/embed/givebackhack-vol-q8n85?fontsize=14&hidenavigation=1&theme=dark" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" title="GiveBackHack Vol" allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow scripts"></iframe>
-
-
 const GiveBackHackVol = () => (
     <>
         <head>
@@ -18,6 +15,7 @@ const GiveBackHackVol = () => (
                     Myself along with a team of developers and businesspeople jointly created a business model and a prototype for a
                     volunteering website.
                 </h3>
+                <iframe src="https://codesandbox.io/embed/givebackhack-vol-q8n85?fontsize=14&hidenavigation=1&theme=dark" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" title="GiveBackHack Vol" allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow scripts"></iframe>
                 <p class="page_description">
                     Vol was intended to be a website that would help users easily find volunteering opportunities in their communities.
                     In my experience it's always been hard to find a centralized place that lists volunteering opportunities. Typically for me
