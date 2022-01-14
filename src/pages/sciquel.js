4a87527eaa5214fec3ae5988ff41836a6de69b26
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-14 17:06:52 -0500

    styling

--- src/pages/sciquel.js
+++ src/pages/sciquel.js
@@ -28,16 +28,11 @@ const Sciquel = () => (
                 <video controls preload="auto" className="page_video">
                     <source src={demoVideo} type="video/mp4"></source>
                 </video>
-                <h3 className="page_subtitle">
-                    The video above shows the state of the website at the point we stopped working on it. The code should be published shortly and I'll provide
-                    a link at that point, but right now it's in the hands of the Sciquel team to publish when they're ready.
-                     My teammate and I worked on this project from the end of December 2021 to the beginning of Januarary 2022 on a part time basis
-                    when we were free during the holiday. Our contact from the Sciquel team created a Figma template which we used as the
-                    starting point for our front end design, but we made changes where we felt it would look better one way versus the other.
-                    We used JavaScript, HTML, CSS, and Bootstrap for the implementation. If we had more time we would have made the website more responsive, integrated the website
-                    with a CMS for easy article publishing, and we would have used Firebase Authentication for the login flow and Firestore to keep track
-                    of some basic concepts such as comments.
-                </h3>
+                <p className="page_description">
+                    The images above show the homepage which is what I worked on mostly. I initially used Bootstrap, but eventually switched to flexbox because I wanted a little bit more
+                    customization on the components. I also worked hard to make this page responsive (as you can see in the video below) which
+                    was especially tricky because of the overlapping components. My teammate worked on header and footer and the rest of the pages screenshotted below.
+                </p>
                 <img className="page_video" src={sciquelhomepage1}></img>
                 <img className="page_video" src={sciquelhomepage2andfooter}></img>
                 <p className="page_description">
