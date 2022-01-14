4a869b5bb3d5b9dcee87fce46c477761328eb6e6
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-14 12:50:52 -0500

    no more table container

--- src/pages/oneworldbrowserextension.js
+++ src/pages/oneworldbrowserextension.js
@@ -446,6 +446,9 @@ let supplyChainOptions = [
 'Services of households as employers of domestic personnel'
 ];
 
+// <TableContainer style={{overflow: "visible"}} component={Paper} id="one_world_table_container">
+
+
 class OneWorldBrowserExtension extends React.Component {
     generateConsumptionOptions(){
 
@@ -608,7 +611,6 @@ class OneWorldBrowserExtension extends React.Component {
                 <video controls className="page_video">
                     <source src={demoVideo} type="video/mp4"></source>
                 </video>
-                <TableContainer style={{overflow: "visible"}} component={Paper} id="one_world_table_container">
                   <Table style={{overflow: "visible"}} aria-label="simple table" id="one_world_table">
                     <TableHead>
                       <TableRow>
@@ -635,7 +637,6 @@ class OneWorldBrowserExtension extends React.Component {
                         )}
                     </TableBody>
                   </Table>
-                </TableContainer>
                 <Button onClick={this.addRowFunction} variant="text" id="one_world_add_row_button"> Add Row </Button>
                 <Button onClick={this.offsetFunction} variant="text" id="one_world_offset_row_button"> Offset CO2 </Button>
                 <p className="page_description">
