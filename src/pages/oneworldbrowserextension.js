5c1a54fe7d1c04617c735587fc942e30798d1723
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-19 23:09:05 -0500

    minor changes

--- src/pages/oneworldbrowserextension.js
+++ src/pages/oneworldbrowserextension.js
@@ -661,8 +661,8 @@ class OneWorldBrowserExtension extends React.Component {
                       </tr>
                     {this.state.rows.map((row) =>
                         <tr key={row.row_num.toString()} style={{overflowY: "visible !important"}}>
-                            <td className="one_world_nameTable"><TextField id="standard-basic" placeholder="Apples..." variant="standard" value={row.name} onChange={(e) => this.nameChange(e.target.value, row.row_num)}/></td>
-                            <td className="one_world_costTable" align="right"><TextField id="standard-basic" placeholder="$10..." variant="standard" value={row.cost} onChange={(e) => this.costChange(e.target.value, row.row_num)}/></td>
+                            <td className="one_world_nameTable"><TextField id="standard-basic" placeholder="Apples..." variant="standard" value={row.name} onChange={(selectedOption) => this.nameChange(selectedOption, row.row_num)}/></td>
+                            <td className="one_world_costTable" align="right"><TextField id="standard-basic" placeholder="$10..." variant="standard" value={row.cost} onChange={(selectedOption) => this.costChange(selectedOption, row.row_num)}/></td>
                             <td className="one_world_submit" align="right"><button onClick={() => this.submitFunction(row.row_num)}>Submit</button></td>
                             <td style={{overflowY: "visible !important"}} className="one_world_consumptionTable" align="right"><Select class="one_world_select" closeMenuOnSelect={false} components={animatedComponentsConsumption} options={selectConsumptionOptions} value={row.consumption} onChange={(e) => this.consumptionChange(e.target.value, row.row_num)}/></td>
                             <td style={{overflowY: "visible !important"}} className="one_world_supplychainTable" align="right"><Select class="one_world_select" closeMenuOnSelect={false} components={animatedComponentsSupplyChain} options={selectSupplyChainOptions} value={row.supply_chain} onChange={(e) => this.supplyChainChange(e.target.value, row.row_num)}/></td>
@@ -674,6 +674,7 @@ class OneWorldBrowserExtension extends React.Component {
                     <tr key="total" style={{overflowY: "visible !important"}}>
                         <td className="one_world_nameTable"></td>
                         <td className="one_world_costTable" align="right"></td>
+                        <td className="one_world_submit" align="right"><button onClick={() => this.submitFunction("total")}>Submit All</button></td>
                         <td style={{overflowY: "visible !important"}} className="one_world_consumptionTable" align="right"></td>
                         <td style={{overflowY: "visible !important"}} className="one_world_supplychainTable" align="right"></td>
                         <td className="one_world_CO2Table" align="right"><div>{this.state.totalKG} kg</div></td>
--- src/pages/smrtnrllbrowserextension.js
+++ src/pages/smrtnrllbrowserextension.js
@@ -22,7 +22,7 @@ const SmrtnrllBrowserExtension = () => (
                 <iframe className="page_video" src="https://www.youtube-nocookie.com/embed/59Mph03CJZU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <p className="page_description">
                     The way it works is by continually refreshing the page and clicking enroll until someone drops the class at which case
-                    you are enrolled. I've used this to succesfully get into 2 classes.
+                    you are enrolled. I've used this to succesfully get into 3 classes and others have used it to varying levels of success.
                 </p>
             </div>
         </body>
