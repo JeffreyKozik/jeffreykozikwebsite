05096858ac1d9d12de895d93093538900d912818
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-21 18:35:26 -0500

    NO BOOTSTRAP

--- src/pages/projects/1world.js
+++ src/pages/projects/1world.js
@@ -2888,9 +2888,6 @@ class OneWorld extends React.Component {
             <>
                 <Helmet>
                     <meta name="viewport" content="initial-scale=1, width=device-width"/>
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-                          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                          crossOrigin="anonymous"/>
                 </Helmet>
                 <body>
                     <Seo title="One World Browser Extension" />
