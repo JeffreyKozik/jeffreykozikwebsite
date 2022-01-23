67be56ec42cad8c3387ded690a2a4937b9ec8236
Jeffrey Kozik jnk58 <jnk58@case.edu>
2022-01-23 16:59:09 -0500

    seeing how pull requests work

--- src/pages/aiproject2.js
+++ src/pages/aiproject2.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 const AiProject2 = () => (
-    <div> AI Project 2 </div>
+    <div> AI Project 2 - seeing how pull requests work</div>
 )
 
 export default AiProject2
