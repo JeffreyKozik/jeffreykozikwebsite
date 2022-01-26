4fbdda64d1e91d2080749bbf2edac7f0602f0728
Jeffrey Kozik <jeffreykozik@protonmail.com>
2022-01-26 15:28:51 -0500

    ../header

--- src/components/pagesHelpers/articlecomponent.js
+++ src/components/pagesHelpers/articlecomponent.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import MediaQuery from 'react-responsive'
-import Header from "../components/header"
+import Header from "../header"
 
 import * as pages from "../../page_styling/pages.module.css"
 
