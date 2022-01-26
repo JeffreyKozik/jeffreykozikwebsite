fd37ba94f843e8b359cb606afe5ce8344071aafc
Jeffrey Kozik <jeffreykozik@protonmail.com>
2022-01-26 16:50:54 -0500

    wworking on some redux stuff

--- src/redux_store/store.js
+++ src/redux_store/store.js
@@ -1,8 +1,8 @@
 // import { configureStore } from "@reduxjs/toolkit";
-// import counterReducer from "../redux_features/portfolioInfo/portfolioInfo";
+// import portfolioNameReducer from "../redux_features/portfolioInfo/portfolioInfo";
 //
 // export default configureStore({
 //   reducer: {
-//     counter: counterReducer
+//     portfolioName: portfolioNameReducer
 //   }
 // });
