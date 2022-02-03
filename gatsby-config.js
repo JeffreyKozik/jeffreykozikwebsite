48c82ac081d48755e569f4a75447c76db0d8d4ba
Jeffrey Kozik <jeffreykozik@protonmail.com>
2022-02-03 09:50:39 -0500

    adding env plugin to see if it works now

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,12 @@ module.exports = {
         icon: `src/images/jeffsquare.jpg`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-env-variables`,
+      options: {
+        allowList: ["AZURE_FUNCTION_URL"]
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
